570f31067c24d6237712f42328feae72921ba8ef
Mafe707 <fernanditarevelo@gmail.com>
2025-04-04 18:04:46 -0500

    onedrive

--- config/database.php
+++ config/database.php
@@ -1,10 +1,19 @@
 <?php
     //Config connection
+    $host       = "aws-0-us-east-1.pooler.supabase.com";
+    $port       = "6543";
+    $dbname     = "postgres";
+    $user       = "postgres.rfjtssxunfeptfohiyld";
+    $password   = "unicesmag@@";
+
+    /*
     $host       = "localhost";
     $port       = "5432";
     $dbname     = "schoolar";
     $user       = "postgres";
     $password   = "unicesmag";
+    */
+
 
     //Create connection
     $conn = pg_connect("
