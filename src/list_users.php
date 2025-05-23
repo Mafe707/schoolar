9afb80d792a190ed56db32ec7c628f5d22dceaff
Mafe707 <fernanditarevelo@gmail.com>
2025-05-23 18:00:41 -0500

    Create add photo

--- src/list_users.php
+++ src/list_users.php
@@ -16,12 +16,14 @@
             <th>Lastname</th>
             <th>E-mail</th>
             <th>Status</th>
+            <th>Photo</th>
             <th>...</th>
         </tr>
     <?php
         //Here code
         $sql = "
             select
+                id,
                 firstname,
                 lastname,
                 email,
@@ -42,9 +44,10 @@
             echo"<td>". $row ['lastname'] ."</td>";
             echo"<td>". $row ['email'] ."</td>";
             echo"<td>". $row ['status'] ."</td>";
+            echo"<td align='center'><img src='photo_users/photo_default.png' width='25'></td>";
             echo"<td>";
             echo"<a href=''><img src = 'icons/edit.png' width='15'></a>";
-            echo"<a href=''><img src = 'icons/dash.png' width='15'></a>";
+            echo"<a href='http://localhost/schoolar/src/delete.php'><img src = 'icons/dash.png' width='15'></a>";
             echo"<a href=''><img src = 'icons/lupa.png' width='15'></a>";
             echo"</td>";
             echo"</tr>";
